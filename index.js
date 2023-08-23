9ab0980f136d8c783cabff81701ac206a12331ff
Gourango Sutradhar <gsrajeni@gmail.com>
2023-08-24 02:13:14 +0600

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Define a global JavaScript function
+function showHtmlData(data) {
+  document.open();
+  document.write(data);
+  document.close();
+}
+
+
+function setCookie(data) {
+  document.cookie = data;
+}
+
